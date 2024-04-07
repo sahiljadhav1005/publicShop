c3218215c3a4d9b3a14214497e84ade9904b73ab
SAHIL JADHAV <121169630+sahiljadhav1005@users.noreply.github.com>
2024-04-07 16:07:09 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,13 @@
+<?php
+
+// $db_name = 'mysql:host=localhost;dbname=shop_db';
+// $user_name = 'root';
+// $user_password = '';
+
+$db_name = 'mysql:host=sql212.infinityfree.com:3306;dbname=if0_36322091_shop_db';
+$user_name = 'if0_36322091';
+$user_password = 'FLyJhiMncfzUb';
+
+$conn = new PDO($db_name, $user_name, $user_password);
+
+?>
---
+++ footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>Quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>Extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> Login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> Register</a>
+         <a href="../admin/admin_login.php"> <i class="fas fa-angle-right"></i>Admin</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>Contact Us.</h3>
+         <a href="tel:9800000000"><i class="fas fa-phone"></i> +977 980 000 0000</a>
+         <a href="tel:9900000000"><i class="fas fa-phone"></i> +977 974 000 0000</a>
+         <a href="mailto:sahiljadhav.pce@gmail.com"><i class="fas fa-envelope"></i>sahiljadhav.pce@gmail.com</a>
+         <a href="https://www.google.com/maps/place/Mankhurd"><i class="fas fa-map-marker-alt"></i> Mumbai , Mankhurd </a>
+      </div>
+
+      <div class="box">
+         <h3>Follow Us</h3>
+         <a href="https://www.facebook.com" target="_blank"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="https://twitter.com/sahiljadhav1005" target="_blank"><i class="fab fa-twitter"></i>Twitter</a>
+         <a href="https://www.instagram.com/_i_am_sahil__10" target="_blank"><i class="fab fa-instagram"></i>Instagram</a>
+         <a href="https://www.linkedin.com/in/sahil-vishwas-jadhav-066447259" target="_blank"><i class="fab fa-linkedin"></i>Linkedin</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>sahil jadhav</span> | all rights reserved!</div>
+
+</footer>
